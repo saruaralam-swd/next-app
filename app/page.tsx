cddbfc9507f0f79019a364ea8a16bbf5fee4cd3a
saruaralam-swd <saruaralam2001@gmail.com>
2023-12-04 14:26:35 +0600

    client and server conpoments implement

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import Image from "next/image";
 import Link from "next/link";
+import ProductCard from "./components/productCard";
 
 export default function Home() {
   return (
     <main>
       <h1>Hello World.</h1>
       <Link href="/users">Users</Link>
+      <ProductCard />
     </main>
   );
 }
